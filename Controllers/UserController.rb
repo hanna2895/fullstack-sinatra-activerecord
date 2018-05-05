54dbd40a3f4fa0055dd6188256d15e3e4208994c
Hannah Werman <hannah.werman@gmail.com>
2018-05-05 14:47:00 -0500

    logout works

--- Controllers/UserController.rb
+++ Controllers/UserController.rb
@@ -38,4 +38,10 @@ class UserController < ApplicationController
 		redirect '/items'
 	end
 
+	get '/logout' do
+		session[:username] = nil
+		session[:logged_in] = false
+		redirect '/user/login'
+	end
+
 end
