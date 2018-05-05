45530debae385fcaf9be8a1e6806fc1519337dde
Hannah Werman <hannah.werman@gmail.com>
2018-05-05 15:11:00 -0500

    added relations and updated item index to use it

--- Controllers/ItemController.rb
+++ Controllers/ItemController.rb
@@ -9,7 +9,9 @@ class ItemController < ApplicationController
 
 	#index route
 	get "/" do
-		@items = Item.all
+		@user = User.find session[:user_id]
+
+		@items = @user.items
 		# @items.to_json
 		@page_title = "Index of Items"
 		erb :item_index
